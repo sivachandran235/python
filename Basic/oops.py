f37545a55e5eead2ea5f42db9c857abf07dd54f9
sivachandran235 <sivachandran235@gmail.com>
2024-06-25 09:54:44 +0530

    basic program

--- Basic/oops.py
+++ Basic/oops.py
@@ -4,4 +4,12 @@ class Student:
 
 s1 = Student()
 s1.name = "ajith"
+<<<<<<< HEAD
 print(s1.name)
+=======
+print(s1.name)
+
+s2=Student()
+s2.name="nithish"
+print(s2.name)
+>>>>>>> b2cfd62 (basic program)
